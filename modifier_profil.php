f39d39890a159f27f596f4e9e711fd14edf514f5
VivienLam <133504212+VivienLam@users.noreply.github.com>
2024-05-26 22:53:26 +0200

    débug photo profil

--- modifier_profil.php
+++ modifier_profil.php
@@ -1,31 +1,28 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Récupération du pseudo de l'utilisateur à modifier
     $pseudo = isset($_POST['pseudo']) ? $_POST['pseudo'] : null;
     $field = isset($_POST['field']) ? $_POST['field'] : null;
     $value = isset($_POST['value']) ? $_POST['value'] : null;
     $filename = 'bdd_users.txt';
     $lines = file($filename, FILE_IGNORE_NEW_LINES);
 
+    $fields = [
+        'Pseudo', 'Sexe', 'ID', 'Date de naissance', 'Profession', 'Pays', 'departement', 'Ville',
+        'Statut marital', 'Enfants', 'Taille (cm)', 'Poids (kg)', 'Description', 'Nom', 'Prénom',
+        'Adresse', 'Date d\'inscription', 'Rôle', 'Photo'
+    ];
+
     if ($pseudo !== null) {
         foreach ($lines as $key => $line) {
             $data = explode(",", $line);
             if ($data[0] === $pseudo) {
-                // Mise à jour du champ spécifique
                 if ($field !== null && $value !== null) {
-                    $fields = [
-                        'Pseudo', 'Sexe', 'ID', 'Date de naissance', 'Profession', 'Pays', 'departement', 'Ville', 
-                        'Statut marital', 'Enfants', 'Taille (cm)', 'Poids (kg)', 'Description', 'Nom', 'Prénom', 
-                        'Adresse', 'Date d\'inscription', 'Rôle', 'Photo'
-                    ];
-
                     $index = array_search($field, $fields);
                     if ($index !== false) {
                         $data[$index] = $value;
                     }
                 }
 
-                // Gestion de la photo de profil
                 if (isset($_FILES['photo']) && $_FILES['photo']['error'] == UPLOAD_ERR_OK) {
                     $photo_name = 'pdp_' . $pseudo . '.jpg';
                     $photo_destination = __DIR__ . '/photos_profil/' . $photo_name;
@@ -38,16 +35,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     }
                 }
 
-                // Mise à jour de la ligne dans le fichier
                 $lines[$key] = implode(",", $data);
                 break;
             }
         }
 
-        // Écriture des modifications dans le fichier
         file_put_contents($filename, implode("\n", $lines));
 
-        // Redirection vers le profil
         header('Location: Profil.php?pseudo=' . urlencode($pseudo));
         exit;
     } else {
